cdeff0b616be463db99b9dced6070697a578fc91
Sam Barker <sambarker247@gmail.com>
2024-07-04 12:23:37 +0100

    2nd collage and image rotation effect

---
+++ assets/js/rotating-images.js
@@ -0,0 +1,20 @@
+document.addEventListener('mousemove', (e) => {
+    const images = document.querySelectorAll('.image-stack__item');
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    // Rotate images towards mouse cursor with CSS perspective
+    images.forEach(image => {
+        const rect = image.getBoundingClientRect();
+        const imageX = rect.left + rect.width / 2;
+        const imageY = rect.top + rect.height / 2;
+
+        const deltaX = e.clientX - imageX;
+        const deltaY = e.clientY - imageY;
+
+        const rotateX = (deltaY / centerY) * -3;
+        const rotateY = -(deltaX / centerX) * -3;
+
+        image.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+});
